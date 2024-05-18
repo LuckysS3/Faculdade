c3db2ef0a64935d58f698df1dfbd75ac4ff62b8c
Luiz Andre <122027228+LuckysS3@users.noreply.github.com>
2024-05-18 15:43:49 -0300

    menu

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int opecao (){
+    int a;
+    printf("Escolha qual questao \n");
+    printf("[1] Qurstao 17 \n");
+    printf("[2] Qurstao 20 \n");
+    printf("[3] Qurstao 22 \n");
+    printf("[4] Qurstao 30 \n");
+    printf("[5] Qurstao 31 \n");
+    scanf("%d",&a);
+    return (a);
+}
+
+
+void main (){
+    int menu;
+    menu = opecao();
+    
+}
